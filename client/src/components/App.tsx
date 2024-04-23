bcb4870a70b5188ce495e4261f77f264df0a4945
AnkeVanBeek-IDgis <anke.van.beek@idgis.nl>
2024-04-23 16:12:51 +0200

    copy basic client-side from Poc_annoteren-met-react

---
+++ client/src/components/App.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import TekstElement from "./TekstElement";
+import "./App.css";
+
+function App() {
+
+    return (
+      <div>
+        <h1>Hello world !</h1>
+        <TekstElement />
+      </div>
+    );
+  }
+  
+  export default App;
+
